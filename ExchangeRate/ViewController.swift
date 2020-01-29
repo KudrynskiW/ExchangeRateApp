2b1e92dbe06f841d066abf483519cbec175f25a5
Wojciech Kudrynski <wojciech.kudrynski@hemmersbach.com>
2020-01-29 16:51:42 +0100

    Basic setup

---
+++ ExchangeRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExchangeRate
+//
+//  Created by Wojciech Kudrynski on 29/01/2020.
+//  Copyright © 2020 Wojciech Kudrynski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
